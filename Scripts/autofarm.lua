406926ac6b5391dc88115497a0962c1c9bb4b810
Karl Wheezer D. God <110885234+InfernoKarl@users.noreply.github.com>
2023-03-16 22:07:01 -0700

    Added auto-challenge

--- Scripts/autofarm.lua
+++ Scripts/autofarm.lua
@@ -1,8 +1,27 @@
--- okay yes this is literally just the code for the killaura but this one teleports you too, so it is way less legit looking than the other one
+-- this one is just killaura + tp to mob + auto challenge
 local roomFolder = game.Workspace.Room
 local challengeFolder = game.Workspace.Challenge
 local remote = game:GetService("ReplicatedStorage").Remote.Event.Fight["[C-S]TakeDamage"]
 local killaurafarm = true
+local player = game.Players.LocalPlayer
+
+
+local function click(a)
+    game:GetService("VirtualInputManager"):SendMouseButtonEvent(a.AbsolutePosition.X+a.AbsoluteSize.X/2,a.AbsolutePosition.Y+50,0,true,a,1)
+    game:GetService("VirtualInputManager"):SendMouseButtonEvent(a.AbsolutePosition.X+a.AbsoluteSize.X/2,a.AbsolutePosition.Y+50,0,false,a,1)
+end
+
+player.PlayerGui.HUD.Right.Show.Frame["\233\135\141\231\148\159"].Button.Red:GetPropertyChangedSignal("Visible"):Connect(function()
+    if player.PlayerGui.HUD.Right.Show.Frame["\233\135\141\231\148\159"].Button.Red then
+        click(player.PlayerGui.Main.Left.AutoAFK.Bg)
+        player.Character.Humanoid.Health = 0
+        wait(3)
+        firesignal(player.PlayerGui.HUD.Right.Show.Frame["\233\135\141\231\148\159"].Button.MouseButton1Click)
+        wait(2)
+        click(player.PlayerGui.Main.Rank.Info.Button.Challenge.Button)
+        click(player.PlayerGui.Main.Left.AutoAFK.Bg)
+    end
+end)
 
 local function killaura()
     local localPlayer = game.Players.LocalPlayer
